8746e2df4aeb5811db3ef6e1f63b2eb17c0c0277
Jem Gold <591643+jemgold@users.noreply.github.com>
2023-05-07 12:32:23 -0700

    breaking change with atproto-react api

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -4,12 +4,19 @@ import type { ReactNode } from "react";
 import { ATPProvider } from "atproto-react/client";
 import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { useAuthFromATP } from "@/hooks/useAuthFromATP";
+import { BskyAgent } from "@atproto/api";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+  verbose: true,
+});
+
+const agent = new BskyAgent({
+  service: "https://bsky.social",
+});
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ATPProvider>
+    <ATPProvider agent={agent}>
       <ConvexProviderWithAuth client={convex} useAuth={useAuthFromATP}>
         {children}
       </ConvexProviderWithAuth>
--- hooks/useAuthFromATP.ts
+++ hooks/useAuthFromATP.ts
@@ -12,28 +12,20 @@ type AuthTranslator = {
 export function useAuthFromATP(): AuthTranslator {
   const { isLoading, isAuthenticated, getToken } = useATP();
 
-  console.log({
-    isLoading,
-    isAuthenticated,
-    getToken,
-  });
-
   const fetchAccessToken = useCallback(
     async ({ forceRefreshToken }: { forceRefreshToken: boolean }) => {
-      // forceRefreshToken doesn't do anything currently
-      // because the ATP client doesn't support it
       const token = await getToken({ ignoreCache: forceRefreshToken });
+
       return token;
     },
     [getToken],
   );
 
-  return useMemo(
-    () => ({
+  return useMemo(() => {
+    return {
       isLoading,
       isAuthenticated: isAuthenticated ?? false,
       fetchAccessToken,
-    }),
-    [fetchAccessToken, isAuthenticated, isLoading],
-  );
+    };
+  }, [fetchAccessToken, isAuthenticated, isLoading]);
 }
